c7821a8fedd878187c7c7c94bec7503157f4451a
Anastasiya Brodskaya <anastasia.brodskaja@yandex.ru>
2023-09-03 22:48:39 +0300

    create function calculate_sum_numbers

--- py_project/new_project/funcs.py
+++ py_project/new_project/funcs.py
@@ -1,3 +1,7 @@
 def calculate_sum_numbers(num_one, num_two):
     total_sum = num_one + num_two
     return total_sum
+
+def substraction_numbers(num_one, num_two):
+    total = num_one - num_two
+    return total
